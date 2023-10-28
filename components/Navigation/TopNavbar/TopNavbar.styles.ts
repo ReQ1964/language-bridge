69b1d731dd7af6275cd1d911b85acf8bc81b9a09
ReQ1964 <slomianykacper4@gmail.com>
2023-10-28 12:38:32 +0200

    feat: :sparkles: add a TopNavbar Layout component, implement globally

---
+++ components/Navigation/TopNavbar/TopNavbar.styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Navbar = styled.nav``;
+
+export default { Navbar };
---
+++ components/Navigation/TopNavbar/TopNavbar.tsx
@@ -0,0 +1,5 @@
+import S from './TopNavbar.styles';
+
+export const TopNavbar = () => {
+	return <S.Navbar></S.Navbar>;
+};
