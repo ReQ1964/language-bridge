aa0f2ea6439149abb17400f07ed8941fe0e9e8de
ReQ1964 <slomianykacper4@gmail.com>
2023-10-28 12:38:32 +0200

    feat: :sparkles: add a TopNavbar Layout component, implement globally

--- components/Navigation/TopNavbar/TopNavbar.styles.ts
+++ components/Navigation/TopNavbar/TopNavbar.styles.ts
@@ -1,31 +1,31 @@
-import styled from 'styled-components';
-import Image from 'next/image';
+import styled from 'styled-components'
+import Image from 'next/image'
 
 const Navbar = styled.nav`
-	position: sticky;
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
-	width: 100%;
-	background-color: white;
-	z-index: 999;
-	padding: 0.8rem 0.1rem;
-`;
+  position: sticky;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  background-color: white;
+  z-index: 999;
+  padding: 0.8rem 0.1rem;
+`
 
 const NextImage = styled(Image)`
-	cursor: pointer;
-	margin-top: 5px;
-`;
+  cursor: pointer;
+  margin-top: 5px;
+`
 
 const RightContainer = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	gap: 0.4rem;
-`;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.4rem;
+`
 
 const AccountIcon = styled(Image)`
-	cursor: pointer;
-`;
+  cursor: pointer;
+`
 
-export default { Navbar, NextImage, RightContainer, AccountIcon };
+export default { Navbar, NextImage, RightContainer, AccountIcon }
