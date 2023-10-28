f994648c3364fbb1db9ddee2dc1ee0a5e97a653d
ReQ1964 <slomianykacper4@gmail.com>
2023-10-28 11:39:29 +0200

    fix: :package: add styled-components compatibility with next.js ssr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: {
+			ssr: true,
+		},
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
