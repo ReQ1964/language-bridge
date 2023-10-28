64761444df7467efc729ee01dea0c5e75d268b9e
ReQ1964 <slomianykacper4@gmail.com>
2023-10-28 12:39:51 +0200

    feat: :sparkles: add BurgerMenu, implement to TopNavbar

--- components/Navigation/BurgerMenu/BurgerMenu.styles.ts
+++ components/Navigation/BurgerMenu/BurgerMenu.styles.ts
@@ -1,35 +1,35 @@
-import styled from 'styled-components';
-import Link from 'next/link';
+import styled from 'styled-components'
+import Link from 'next/link'
 
 const UnorderedList = styled.ul.withConfig({
-	shouldForwardProp: (prop) => prop !== 'isOpen',
+  shouldForwardProp: (prop) => prop !== 'isOpen',
 })<{ isOpen: boolean }>`
-	position: fixed;
-	display: flex;
-	flex-direction: column;
-	left: 0%;
-	top: -20%;
-	text-align: center;
-	list-style: none;
-	background-color: white;
-	width: 100%;
-	z-index: 1;
-	transition: 0.8s;
-	transform: translateY(${(props) => (props.isOpen ? '130%' : '-40%')});
-`;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  left: 0%;
+  top: -20%;
+  text-align: center;
+  list-style: none;
+  background-color: white;
+  width: 100%;
+  z-index: 1;
+  transition: 0.8s;
+  transform: translateY(${(props) => (props.isOpen ? '130%' : '-40%')});
+`
 
 const NextLink = styled(Link)`
-	font-size: 1.3rem;
-	transition: border 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  font-size: 1.3rem;
+  transition: border 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
 
-	&:hover {
-		color: ${(props) => props.theme.colors.text.primary};
-		border-bottom: 1px solid ${(props) => props.theme.colors.text.primary};
-	}
-`;
+  &:hover {
+    color: ${(props) => props.theme.colors.text.primary};
+    border-bottom: 1px solid ${(props) => props.theme.colors.text.primary};
+  }
+`
 
 const ListItem = styled.li`
-	padding: 0.7rem 0;
-`;
+  padding: 0.7rem 0;
+`
 
-export default { UnorderedList, NextLink, ListItem };
+export default { UnorderedList, NextLink, ListItem }
--- components/Navigation/BurgerMenu/BurgerMenu.tsx
+++ components/Navigation/BurgerMenu/BurgerMenu.tsx
@@ -1,19 +1,21 @@
-import S from './BurgerMenu.styles';
-import routePages from '@/config/routePages';
-import capitalizeWord from '@/utils/capitalizeWord';
+import S from './BurgerMenu.styles'
+import routePages from '@/config/routePages'
+import capitalizeWord from '@/utils/capitalizeWord'
 
 const BurgerMenu = ({ isOpen }: { isOpen: boolean }) => {
-	return (
-		<div>
-			<S.UnorderedList isOpen={isOpen}>
-				{routePages.map((page) => (
-					<S.ListItem key={page.name}>
-						<S.NextLink href={page.path}>{capitalizeWord(page.name)}</S.NextLink>
-					</S.ListItem>
-				))}
-			</S.UnorderedList>
-		</div>
-	);
-};
+  return (
+    <div>
+      <S.UnorderedList isOpen={isOpen}>
+        {routePages.map((page) => (
+          <S.ListItem key={page.name}>
+            <S.NextLink href={page.path}>
+              {capitalizeWord(page.name)}
+            </S.NextLink>
+          </S.ListItem>
+        ))}
+      </S.UnorderedList>
+    </div>
+  )
+}
 
-export default BurgerMenu;
+export default BurgerMenu
