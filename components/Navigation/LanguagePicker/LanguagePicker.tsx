025bd01bf6a4a53a3f8eec236e74c38d240d5f6b
ReQ1964 <slomianykacper4@gmail.com>
2023-10-30 19:17:49 +0100

    feat: :sparkles: add LanguagePicker and icons for it, implement to TopNavbar

--- components/Navigation/LanguagePicker/LanguagePicker.tsx
+++ components/Navigation/LanguagePicker/LanguagePicker.tsx
@@ -1,54 +1,54 @@
-import type { MenuProps } from 'antd';
-import Image from 'next/image';
-import { Dropdown } from 'antd';
-import gb_icon from '@/public/icons/flags/gb.svg';
-import es_icon from '@/public/icons/flags/es.svg';
-import show_more_icon from '@/public/icons/navigation/menu_show_more.svg';
-import S from './LanguagePicker.style';
+import type { MenuProps } from 'antd'
+import Image from 'next/image'
+import { Dropdown } from 'antd'
+import gb_icon from '@/public/icons/flags/gb.svg'
+import es_icon from '@/public/icons/flags/es.svg'
+import show_more_icon from '@/public/icons/navigation/menu_show_more.svg'
+import S from './LanguagePicker.style'
 
 const items: MenuProps['items'] = [
-	{
-		key: '1',
-		label: (
-			<S.DropdownContainer>
-				<Image
-					src={gb_icon}
-					width={22}
-					height={22}
-					alt="Flag of the United Kingdom"
-				/>
-				<p>ENG</p>
-			</S.DropdownContainer>
-		),
-	},
-	{
-		key: '2',
-		label: (
-			<S.DropdownContainer>
-				<Image src={es_icon} width={22} height={22} alt="Flag of Spain" />
-				<p>SPA</p>
-			</S.DropdownContainer>
-		),
-	},
-];
+  {
+    key: '1',
+    label: (
+      <S.DropdownContainer>
+        <Image
+          src={gb_icon}
+          width={22}
+          height={22}
+          alt='Flag of the United Kingdom'
+        />
+        <p>ENG</p>
+      </S.DropdownContainer>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <S.DropdownContainer>
+        <Image src={es_icon} width={22} height={22} alt='Flag of Spain' />
+        <p>SPA</p>
+      </S.DropdownContainer>
+    ),
+  },
+]
 
 const LanguagePicker = () => (
-	<Dropdown menu={{ items }} placement="bottom">
-		<S.DropdownButton>
-			<Image
-				src={gb_icon}
-				width={24}
-				height={24}
-				alt="Flag of the United Kingdom"
-			/>
-			<Image
-				src={show_more_icon}
-				width={24}
-				height={24}
-				alt="Click to show languages to choose"
-			/>
-		</S.DropdownButton>
-	</Dropdown>
-);
+  <Dropdown menu={{ items }} placement='bottom'>
+    <S.DropdownButton>
+      <Image
+        src={gb_icon}
+        width={24}
+        height={24}
+        alt='Flag of the United Kingdom'
+      />
+      <Image
+        src={show_more_icon}
+        width={24}
+        height={24}
+        alt='Click to show languages to choose'
+      />
+    </S.DropdownButton>
+  </Dropdown>
+)
 
-export default LanguagePicker;
+export default LanguagePicker
