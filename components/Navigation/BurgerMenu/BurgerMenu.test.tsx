ecb083a7541046b04ac4e923541654287ea0d2be
ReQ1964 <slomianykacper4@gmail.com>
2023-10-31 20:55:37 +0100

    feat: :white_check_mark: add test for BurgerMenu

--- components/Navigation/BurgerMenu/BurgerMenu.test.tsx
+++ components/Navigation/BurgerMenu/BurgerMenu.test.tsx
@@ -1,73 +1,73 @@
-import { render, screen } from '@testing-library/react';
-import ThemesProvider from '@/theme/ThemesProvider';
-import { composeStories } from '@storybook/react';
-import * as stories from './BurgerMenu.stories';
+import { render, screen } from '@testing-library/react'
+import ThemesProvider from '@/theme/ThemesProvider'
+import { composeStories } from '@storybook/react'
+import * as stories from './BurgerMenu.stories'
 
-const { Open, Close } = composeStories(stories);
+const { Open, Close } = composeStories(stories)
 
 describe('BurgerMenu', () => {
-	const linkTexts = ['Home', 'Contact', 'About', 'Texts'];
+  const linkTexts = ['Home', 'Contact', 'About', 'Texts']
 
-	it('should render the menu with links and be in view when isOpen is true', () => {
-		render(
-			<ThemesProvider>
-				<Open />
-			</ThemesProvider>
-		);
+  it('should render the menu with links and be in view when isOpen is true', () => {
+    render(
+      <ThemesProvider>
+        <Open />
+      </ThemesProvider>
+    )
 
-		linkTexts.forEach((text) => {
-			const link = screen.getByText(text);
-			expect(link).toBeInTheDocument();
-		});
+    linkTexts.forEach((text) => {
+      const link = screen.getByText(text)
+      expect(link).toBeInTheDocument()
+    })
 
-		const menu = screen.getByRole('list');
-		expect(menu).toHaveStyle(`transform: translateY(130%)`);
-	});
+    const menu = screen.getByRole('list')
+    expect(menu).toHaveStyle(`transform: translateY(130%)`)
+  })
 
-	it('should render the menu with links and be out of view when isOpen is false', () => {
-		render(
-			<ThemesProvider>
-				<Close />
-			</ThemesProvider>
-		);
+  it('should render the menu with links and be out of view when isOpen is false', () => {
+    render(
+      <ThemesProvider>
+        <Close />
+      </ThemesProvider>
+    )
 
-		linkTexts.forEach((text) => {
-			const link = screen.getByText(text);
-			expect(link).toBeInTheDocument();
-		});
+    linkTexts.forEach((text) => {
+      const link = screen.getByText(text)
+      expect(link).toBeInTheDocument()
+    })
 
-		const menu = screen.getByRole('list');
-		expect(menu).toHaveStyle(`transform: translateY(-40%)`);
-	});
+    const menu = screen.getByRole('list')
+    expect(menu).toHaveStyle(`transform: translateY(-40%)`)
+  })
 
-	it('should check if links have correct paths', () => {
-		render(
-			<ThemesProvider>
-				<Open />
-			</ThemesProvider>
-		);
+  it('should check if links have correct paths', () => {
+    render(
+      <ThemesProvider>
+        <Open />
+      </ThemesProvider>
+    )
 
-		linkTexts.forEach((text) => {
-			const link = screen.getByText(text);
-			if (text === 'Home') {
-				expect(link).toHaveAttribute('href', '/');
-			} else {
-				expect(link).toHaveAttribute('href', `/${text.toLowerCase()}`);
-			}
-		});
-	});
+    linkTexts.forEach((text) => {
+      const link = screen.getByText(text)
+      if (text === 'Home') {
+        expect(link).toHaveAttribute('href', '/')
+      } else {
+        expect(link).toHaveAttribute('href', `/${text.toLowerCase()}`)
+      }
+    })
+  })
 
-	it('should capitalize the first letter of each menu item', () => {
-		render(
-			<ThemesProvider>
-				<Open />
-			</ThemesProvider>
-		);
+  it('should capitalize the first letter of each menu item', () => {
+    render(
+      <ThemesProvider>
+        <Open />
+      </ThemesProvider>
+    )
 
-		linkTexts.forEach((text) => {
-			const link = screen.getByText(text);
-			expect(link.textContent).toBe(text);
-			expect(link.textContent?.charAt(0)).toMatch(/[A-Z]/);
-		});
-	});
-});
+    linkTexts.forEach((text) => {
+      const link = screen.getByText(text)
+      expect(link.textContent).toBe(text)
+      expect(link.textContent?.charAt(0)).toMatch(/[A-Z]/)
+    })
+  })
+})
