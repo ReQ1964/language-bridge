bd506f54a770d63d44ba8e50168220d4d6be9504
ReQ1964 <slomianykacper4@gmail.com>
2023-11-01 11:39:40 +0100

    feat: :white_check_mark: add tests to LanguagePicker

--- components/Navigation/LanguagePicker/LanguagePicker.test.tsx
+++ components/Navigation/LanguagePicker/LanguagePicker.test.tsx
@@ -1,27 +1,27 @@
-import LanguagePicker from './LanguagePicker';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { composeStories } from '@storybook/react';
-import * as stories from './LanguagePicker.stories';
+import LanguagePicker from './LanguagePicker'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { composeStories } from '@storybook/react'
+import * as stories from './LanguagePicker.stories'
 
-const { Default } = composeStories(stories);
+const { Default } = composeStories(stories)
 
 describe('LanguagePicker', () => {
-	beforeEach(() => {
-		render(<Default />);
-	});
+  beforeEach(() => {
+    render(<Default />)
+  })
 
-	it('should respond to hover actions; show/hide', async () => {
-		const button = screen.getByRole('button');
+  it('should respond to hover actions; show/hide', async () => {
+    const button = screen.getByRole('button')
 
-		await userEvent.hover(button);
-		await waitFor(() => {
-			expect(button).toHaveClass('ant-dropdown-open');
-		});
+    await userEvent.hover(button)
+    await waitFor(() => {
+      expect(button).toHaveClass('ant-dropdown-open')
+    })
 
-		await userEvent.unhover(button);
-		await waitFor(() => {
-			expect(button).not.toHaveClass('ant-dropdown-open');
-		});
-	});
-});
+    await userEvent.unhover(button)
+    await waitFor(() => {
+      expect(button).not.toHaveClass('ant-dropdown-open')
+    })
+  })
+})
