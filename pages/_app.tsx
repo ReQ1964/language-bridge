9374442c76508bfb639ad9361148ccb163d8392f
ReQ1964 <slomianykacper4@gmail.com>
2023-11-01 17:09:43 +0100

    style: :lipstick: add global AntD ThemeConfig; button hover color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,23 @@ import '@/styles/globals.scss';
 import '@/styles/variables.scss';
 import type { AppProps } from 'next/app';
 import Layout from '@/layouts/Layout';
+import { ConfigProvider } from 'antd';
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+	components: {
+		Button: {
+			colorPrimaryHover: '#765612',
+		},
+	},
+};
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<ConfigProvider theme={theme}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ConfigProvider>
 	);
 }
