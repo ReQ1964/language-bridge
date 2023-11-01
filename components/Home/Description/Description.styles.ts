a3877df51b7309b03aeba1f8775af95058197df6
ReQ1964 <slomianykacper4@gmail.com>
2023-11-01 17:56:09 +0100

    feat: :sparkles: add Description, stories for it, implement to index.tsx

---
+++ components/Home/Description/Description.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const DescriptionSection = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	gap: 1rem;
+	padding: 3rem 0.8rem;
+	margin: 2rem 0 2rem 0;
+	border-top: 1px solid #e6e6e6;
+	border-bottom: 1px solid #e6e6e6;
+	h1 {
+		color: var(--primary-color);
+	}
+`;
+
+export default { DescriptionSection };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import PremiumDiscount from '@/components/Home/PremiumDiscount/PremiumDiscount';
 import Hero from '@/components/Home/Hero/Hero';
+import Description from '@/components/Home/Description/Description';
 
 const HomePage = () => {
 	return (
 		<>
 			<PremiumDiscount />
 			<Hero />
+			<Description />
 		</>
 	);
 };
