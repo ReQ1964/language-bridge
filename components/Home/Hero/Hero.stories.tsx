0b4ed3e11eff8acf464e12579856109bf79279a6
ReQ1964 <slomianykacper4@gmail.com>
2023-11-01 17:16:17 +0100

    feat: :sparkles: add stories for Hero

--- components/Home/Hero/Hero.stories.tsx
+++ components/Home/Hero/Hero.stories.tsx
@@ -1,14 +1,14 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react'
 
-import Hero from './Hero';
+import Hero from './Hero'
 
 const meta: Meta<typeof Hero> = {
-	component: Hero,
-};
+  component: Hero,
+}
 
-export default meta;
-type Story = StoryObj<typeof Hero>;
+export default meta
+type Story = StoryObj<typeof Hero>
 
 export const Default: Story = {
-	render: () => <Hero />,
-};
+  render: () => <Hero />,
+}
