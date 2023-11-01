8a0e827e7d5d863301fa09d75484039b844d628d
ReQ1964 <slomianykacper4@gmail.com>
2023-11-01 09:52:59 +0100

    fix: :wrench: add integration for absolute paths to vitest and storybook
    storybook install nextjs addon

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './tests/setup.ts',
-	},
-});
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
