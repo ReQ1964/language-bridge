ac387800ccc45778f0230b8bac62c0bc1da20d05
ReQ1964 <slomianykacper4@gmail.com>
2023-11-01 16:02:41 +0100

    feat: :sparkles: add PremiumDiscount and implement to index.ts (Home)

--- components/Home/PremiumDiscount/PremiumDiscount.tsx
+++ components/Home/PremiumDiscount/PremiumDiscount.tsx
@@ -1,11 +1,13 @@
-import S from './PremiumDiscount.styles';
+import S from './PremiumDiscount.styles'
 
 const PremiumDiscount = () => {
-	return (
-		<S.DiscountContainer>
-			<p>Unlock new language superpowers with 50% off LanguageBridge Premium!</p>
-		</S.DiscountContainer>
-	);
-};
+  return (
+    <S.DiscountContainer>
+      <p>
+        Unlock new language superpowers with 50% off LanguageBridge Premium!
+      </p>
+    </S.DiscountContainer>
+  )
+}
 
-export default PremiumDiscount;
+export default PremiumDiscount
