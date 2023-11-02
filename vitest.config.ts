b889959f3a3203b2bfe3d6180e692828e94920a9
ReQ1964 <slomianykacper4@gmail.com>
2023-11-02 20:50:11 +0100

    fix: :wrench: use plugin for vitest '@' aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
 		setupFiles: './tests/setup.ts',
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
 	},
 });
