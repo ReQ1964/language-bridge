b7fde9e25c2458642f01807f26770fb22a797334
ReQ1964 <slomianykacper4@gmail.com>
2023-11-04 11:29:07 +0100

    fix: :bug: introduce styled-components theme and vitest compatibility

--- components/Navigation/TopNavbar/TopNavbar.test.tsx
+++ components/Navigation/TopNavbar/TopNavbar.test.tsx
@@ -1,41 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import ThemesProvider from '@/theme/ThemesProvider';
-import { composeStories } from '@storybook/react';
-import * as stories from './TopNavbar.stories';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import ThemesProvider from '@/theme/ThemesProvider'
+import { composeStories } from '@storybook/react'
+import * as stories from './TopNavbar.stories'
 
-const { Default } = composeStories(stories);
+const { Default } = composeStories(stories)
 
 describe('TopNavbar', () => {
-	beforeEach(() => {
-		render(
-			<ThemesProvider>
-				<Default />
-			</ThemesProvider>
-		);
-	});
+  beforeEach(() => {
+    render(
+      <ThemesProvider>
+        <Default />
+      </ThemesProvider>
+    )
+  })
 
-	it('should render the navbar', () => {
-		const navbar = screen.getByRole('navigation');
-		expect(navbar).toBeInTheDocument();
-	});
+  it('should render the navbar', () => {
+    const navbar = screen.getByRole('navigation')
+    expect(navbar).toBeInTheDocument()
+  })
 
-	it('should slide navigation links into screen when BurgerIcon is clicked', async () => {
-		const burgerIcon = screen.getByAltText(/icon to open the navigation bar/i);
-		await userEvent.click(burgerIcon);
+  it('should slide navigation links into screen when BurgerIcon is clicked', async () => {
+    const burgerIcon = screen.getByAltText(/icon to open the navigation bar/i)
+    await userEvent.click(burgerIcon)
 
-		const navLinks = screen.getByRole('list');
-		expect(navLinks).toHaveStyle(`transform: translateY(130%)`);
-	});
+    const navLinks = screen.getByRole('list')
+    expect(navLinks).toHaveStyle(`transform: translateY(130%)`)
+  })
 
-	it('should slide navigation links out of the screen when CloseIcon is clicked', async () => {
-		const burgerIcon = screen.getByAltText(/icon to open the navigation bar/i);
-		await userEvent.click(burgerIcon);
+  it('should slide navigation links out of the screen when CloseIcon is clicked', async () => {
+    const burgerIcon = screen.getByAltText(/icon to open the navigation bar/i)
+    await userEvent.click(burgerIcon)
 
-		const closeIcon = screen.getByAltText(/icon to close the navigation bar/i);
-		await userEvent.click(closeIcon);
+    const closeIcon = screen.getByAltText(/icon to close the navigation bar/i)
+    await userEvent.click(closeIcon)
 
-		const navLinks = screen.getByRole('list');
-		expect(navLinks).toHaveStyle(`transform: translateY(-40%)`);
-	});
-});
+    const navLinks = screen.getByRole('list')
+    expect(navLinks).toHaveStyle(`transform: translateY(-40%)`)
+  })
+})
