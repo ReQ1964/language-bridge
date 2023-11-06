dae4f12d9c73f09b230e5686caaeb6dce0a38d72
ReQ1964 <slomianykacper4@gmail.com>
2023-11-06 20:01:23 +0100

    feat: :sparkles: integrate antD, styled-components with storybook, improve Theme Providing
    Theme Provider now is a separate function for app, tests and themedecorator

--- helpers/MockTheme.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeProvider } from 'styled-components';
-import globalTheme from '@/styles/globalTheme';
-
-function MockTheme({ children }: any) {
-	return <ThemeProvider theme={globalTheme}>{children}</ThemeProvider>;
-}
-
-export default MockTheme;
