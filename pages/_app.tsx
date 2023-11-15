a898045654b1d938ec9490ad5d45bd346a54013c
ReQ1964 <slomianykacper4@gmail.com>
2023-11-15 20:01:32 +0100

    feat: :package: install and 'provide' Tanstack Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,18 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '@/layouts/Layout'
 import ThemesProvider from '@/theme/ThemesProvider'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
-    <ThemesProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemesProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemesProvider>
+    </QueryClientProvider>
   )
 }
